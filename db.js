fa75b6933001911d098b561f92c1fdc11433bda3
salonidhatrak <salonidhatrak123@gmail.com>
2023-07-21 22:46:58 +0530

    dummy data

--- db.js
+++ db.js
@@ -1,15 +1,105 @@
 {
-    "books"; [
-      {
-        "id": 1,
-        "image_url": "https://m.media-amazon.com/images/I/5165He67NEL.jpg",
-        "book_name": "Harry Potter and the Philosopher's Stone",
-        "author": "J. K. Rowling",
-        "genre": "fiction",
-        "edition": "2021",
-        "publisher": "Bloomsbury",
-        "cost": 1299,
-        "borrowed": false
-      }
+    "cars": [
+        {
+            "id": 1,
+            "brand": "Tata",
+            "type": "Manual",
+            "year": 2019,
+            "kms": 10000,
+            "Description": "Tata Altroz with all original documents",
+            "Price": 500000
+        },
+        {
+            "id": 2,
+            "brand": "Tata",
+            "type": "AMT",
+            "year": 2017,
+            "kms": 8000,
+            "Description": "Tata Nexon with all original documents",
+            "Price": 800000
+        },
+        {
+            "id": 3,
+            "brand": "Maruti",
+            "type": "AMT",
+            "year": 2020,
+            "kms": 8000,
+            "Description": "Tata Nexon with all original documents",
+            "Price": 1800000
+        },
+        {
+            "id": 2,
+            "brand": "Mahendra",
+            "type": "AMT",
+            "year": 2000,
+            "kms": 8000,
+            "Description": "Tata Nexon with all original documents",
+            "Price": 8000
+        }
+    ],
+    "user": [
+        {
+            "id": 1,
+            "name": "saloni",
+            "email": "s@gmail.com",
+            "password": 123,
+            "task": [
+                {
+                    "t1": "attend mock",
+                    "t2": "attend interview",
+                    "t3": "revision",
+                    "t4": "breakfast",
+                    "t5": "sleep"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "name": "rani",
+            "email": "r@gmail.com",
+            "password": 1234,
+            "task": [
+                {
+                    "t1": "attend mock",
+                    "t2": "attend interview",
+                    "t3": "revision  react",
+                    "t4": "dinner",
+                    "t5": "sleep"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "name": "nilay",
+            "email": "n@gmail.com",
+            "password": 12345,
+            "task": [
+                {
+                    "t1": "study history",
+                    "t2": "attend interview",
+                    "t3": "revision of history",
+                    "t4": "palyng footboll",
+                    "t5": "sleep"
+                }
+            ]
+        },
+        {
+            "id": 1,
+            "name": "kalyani",
+            "email": "k@gmail.com",
+            "password": 12,
+            "task": [
+                {
+                    "t1": "shepping ",
+                    "t2": "attend interview",
+                    "t3": "commit work",
+                    "t4": "lunch",
+                    "t5": "sleep"
+                }
+            ]
+        }
     ]
-  }
+    
+        
+    
+}
